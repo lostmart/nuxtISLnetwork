0abdc2e62b8e52778b04c086f98068087faf21bd
lostmart <lostmart@hotmail.com>
2022-09-15 18:17:55 +0200

    new pages

---
+++ components/AccordeonComp.vue
@@ -0,0 +1,72 @@
+<template>
+  <div class="accordion" id="FAQaccordion">
+    <div class="accordion-item">
+      <h2 class="accordion-header" :id="id">
+        <button
+          class="accordion-button"
+          type="button"
+          data-bs-toggle="collapse"
+          :data-bs-target="`#${collapsNo}`"
+          aria-expanded="true"
+          :aria-controls="collapsNo"
+        >
+          {{ title }}
+        </button>
+      </h2>
+      <div
+        :id="collapsNo"
+        class="accordion-collapse collapse"
+        :aria-labelledby="id"
+        data-bs-parent="#FAQaccordion"
+      >
+        <div class="accordion-body">
+          <ul class="nav nav-tabs">
+            <li class="nav-item">
+              <button
+                @click="language = 'spanish'"
+                class="nav-link"
+                :class="{ active: language === 'spanish' }"
+                aria-current="page"
+              >
+                ES
+              </button>
+            </li>
+            <li class="nav-item">
+              <button
+                @click="language = 'english'"
+                :class="{ active: language === 'english' }"
+                class="nav-link"
+                aria-current="page"
+              >
+                EN
+              </button>
+            </li>
+          </ul>
+          <FaqOneSp v-if="language === 'spanish' && id === 'headingOne'" />
+          <FaqOneEng v-if="language === 'english' && id === 'headingOne'" />
+          <FaqTwoSp v-if="language === 'spanish' && id === 'headingTwo'" />
+          <FaqTwoEng v-if="language === 'english' && id === 'headingTwo'" />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["title", "id", "collapsNo"],
+  data() {
+    return {
+      language: "spanish",
+    };
+  },
+};
+</script>
+
+<style scoped>
+.accordion-button {
+  color: #ff5722 !important;
+  caret-color: #ff5722 !important;
+  font-weight: bold;
+}
+</style>
--- pages/faq.vue
+++ pages/faq.vue
@@ -1,6 +1,17 @@
 <template>
   <div class="container">
-    <h2>faq</h2>
+    <h2 class="text-center">FAQ</h2>
+    <AccordeonComp
+      title="What is Inglés Sin Límites? / ¿Qué es Inglés Sin Límites?"
+      id="headingOne"
+      collapsNo="collapseOne"
+    />
+    <AccordeonComp
+      title="What is the Godparents Project? / ¿Qué es el Proyecto de
+								Padrinos y Madrinas?"
+      id="headingTwo"
+      collapsNo="collapseTwo"
+    />
   </div>
 </template>
 
